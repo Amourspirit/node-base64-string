2dbb3f418eaba0ce42f73eb5ded1ac349c33291a
Paul Moss <bigbytetech@gmail.com>
2019-04-07 22:05:26 -0400

    Updated cli

--- bin/base64.js
+++ bin/base64.js
@@ -1,38 +1,69 @@
 #!/usr/bin/env node
-// #region requires
+"use strict";
+// #region imports
 var base64 = require('../index').Base64;
-var fs = require('fs');
-var path = require('path');
-var mkdirp = require('mkdirp');
-var endOfLine = require('os').EOL;
-// #endregion
-// #region options
+var fs = require("fs");
+var path = require("path");
+var mkdirp = require("mkdirp");
+var os = require("os");
+var endOfLine = os.EOL;
 var options = {
-  in: false, // input file
-  out: false, // output file
+  in: false,
+  out: false,
   decode: false,
   encode: true,
   encodeUrl: false,
   sniff: false,
-  s: false, // text to encode
+  s: false,
   eol: false // determins if end of line should be included with output
 };
+// #endregion
+// #region functions
+var readFileContents = function (filePath) {
+  var s = fs.readFileSync(filePath).toString();
+  return s;
+};
+var writeFilecontents = function (filePath, contents) {
+  var p = path.dirname(filePath);
+  mkdirp.sync(p);
+  fs.writeFileSync(filePath, contents);
+};
+var resolveHome = function (filepath) {
+  if (filepath[0] === '~' && process.env.HOME) {
+    return path.join(process.env.HOME, filepath.slice(1));
+  }
+  return filepath;
+};
+var log = function (value, eol) {
+  // use stdout instead of console.log
+  // console.log adds a newline \n
+  if (eol === true) {
+    value += endOfLine;
+  }
+  process.stdout.write(value);
+};
+// #endregion
 // #region read params
+// tslint:disable-next-line: one-variable-per-declaration
 for (var i = 2, ii = process.argv.length; i < ii; i++) {
   var arg = process.argv[i];
-
+  var str = '';
   if (arg.search('input:') >= 0) {
-    var str = arg.split(':')[1];
-    if (str) { options.s = str; }
-  } else if (arg.search('in:') >= 0) {
-    var str = arg.split(':')[1] + '';
+    str = arg.split(':')[1];
+    if (str) {
+      options.s = str;
+    }
+  }
+  else if (arg.search('in:') >= 0) {
+    str = arg.split(':')[1] + '';
     if (str.length > 0) {
       str = resolveHome(str);
       str = path.resolve(str);
       options.in = str;
     }
-  } else if (arg.search('out:') >= 0) {
-    var str = arg.split(':')[1] + '';
+  }
+  else if (arg.search('out:') >= 0) {
+    str = arg.split(':')[1] + '';
     if (str.length > 0) {
       str = resolveHome(str);
       str = path.resolve(str);
@@ -53,80 +84,102 @@ for (var i = 2, ii = process.argv.length; i < ii; i++) {
   else if (arg.search('sniff') >= 0) {
     options.sniff = true;
     options.encode = false;
-  } else if (arg.search('eol') >= 0) {
+  }
+  else if (arg.search('eol') >= 0) {
     options.eol = true;
   }
-};
-// #endregion
+}
+;
 // #endregion
 // #region process
 var b64 = new base64();
 var result = '';
 if (options.decode === true) {
   if (options.in) {
-    if (fs.existsSync(options.in)) {
+    if (fs.existsSync(options.in.toString())) {
       try {
-        result = readFileContents(options.in);
+        result = readFileContents(options.in.toString());
         result = b64.decode(result);
-      } catch (error) {
+      }
+      catch (error) {
+        // tslint:disable-next-line: no-console
         console.error(error);
         process.exit(1);
       }
-    } else {
+    }
+    else {
+      // tslint:disable-next-line: no-console
       console.error('File specified for in: parameter not found');
       process.exit(1);
     }
-  } else if (options.s) {
-    result = b64.decode(options.s);
   }
-} else if (options.encode === true) {
+  else if (options.s) {
+    result = b64.decode(options.s.toString());
+  }
+}
+else if (options.encode === true) {
   if (options.in) {
-    if (fs.existsSync(options.in)) {
+    if (fs.existsSync(options.in.toString())) {
       try {
-        result = readFileContents(options.in);
+        result = readFileContents(options.in.toString());
         result = b64.encode(result);
-      } catch (error) {
+      }
+      catch (error) {
+        // tslint:disable-next-line: no-console
         console.error(error);
         process.exit(1);
       }
-    } else {
+    }
+    else {
+      // tslint:disable-next-line: no-console
       console.error('File specified for in: parameter not found');
       process.exit(1);
     }
-  } else if (options.s) {
-    result = b64.encode(options.s);
   }
-} else if (options.encodeUrl === true) {
+  else if (options.s) {
+    result = b64.encode(options.s.toString());
+  }
+}
+else if (options.encodeUrl === true) {
   if (options.in) {
-    if (fs.existsSync(options.in)) {
+    if (fs.existsSync(options.in.toString())) {
       try {
-        result = readFileContents(options.in);
+        result = readFileContents(options.in.toString());
         result = b64.urlEncode(result);
-      } catch (error) {
+      }
+      catch (error) {
+        // tslint:disable-next-line: no-console
         console.error(error);
         process.exit(1);
       }
-    } else {
+    }
+    else {
+      // tslint:disable-next-line: no-console
       console.error('File specified for in: parameter not found');
       process.exit(1);
     }
-  } else if (options.s) {
-    result = b64.urlEncode(options.s);
   }
-} else if (options.sniff === true) {
+  else if (options.s) {
+    result = b64.urlEncode(options.s.toString());
+  }
+}
+else if (options.sniff === true) {
   result = 'false';
-  if (fs.existsSync(options.in)) {
-      try {
-        var sniffString = readFileContents(options.in);
-        if (b64.urlSniff(sniffString)) {
-          result = 'true';
-        }
-      } catch (error) {
-        console.error(error);
-        process.exit(1);
+  if (fs.existsSync(options.in.toString())) {
+    try {
+      var sniffString = readFileContents(options.in.toString());
+      if (b64.urlSniff(sniffString)) {
+        result = 'true';
       }
-    } else if (options.s) {
-    if (b64.urlSniff(options.s)) {
+    }
+    catch (error) {
+      // tslint:disable-next-line: no-console
+      console.error(error);
+      process.exit(1);
+    }
+  }
+  else if (options.s) {
+    if (b64.urlSniff(options.s.toString())) {
       result = 'true';
     }
   }
@@ -135,43 +188,23 @@ if (options.decode === true) {
 // #region do work
 if (options.out) {
   try {
-    writeFilecontents(options.out, result);
+    writeFilecontents(options.out.toString(), result);
     process.exit(0);
-  } catch (error) {
+  }
+  catch (error) {
+    // tslint:disable-next-line: no-console
     console.error(error);
     process.exit(1);
   }
-} else if (options.s) {
+}
+else if (options.s) {
   log(result, options.eol);
-} else {
+}
+else {
   log(result, options.eol);
 }
 // #endregion
 // #region exit
 process.exit(0);
 // #endregion
-// #region functions
-function readFileContents(filePath) {
-  var str = fs.readFileSync(filePath).toString();
-  return str;
-}
-function writeFilecontents(filePath, contents) {
-  var p = path.dirname(filePath);
-  mkdirp.sync(p);
-  fs.writeFileSync(filePath, contents);
-}
-function resolveHome(filepath) {
-  if (filepath[0] === '~') {
-    return path.join(process.env.HOME, filepath.slice(1));
-  }
-  return filepath;
-}
-function log(value, eol) {
-  // use stdout instead of console.log
-  // console.log adds a newline \n
-  if (eol === true) {
-    value += endOfLine;
-  }
-  process.stdout.write(value);
-}
-// #endregion
+//# sourceMappingURL=cli.js.map
